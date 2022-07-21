0e8cea1a02ab7440f0e243ca4a3f1c77271426ac
jorgeeder <jorgeeder.js@gmail.com>
2022-07-21 18:11:59 -0300

    feat: authentication with email and password in FireBase

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import { Heading, VStack, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 
@@ -10,13 +12,39 @@ import { Button } from "../components/Button";
 
 
 export function SignIn() {
-    const [name, setName] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const { colors } = useTheme();
 
     function handleSignIn() {
-        console.log(name, password);
+        if(!email || ! password){
+            return Alert.alert("Entrar", "Informe e-mail e senha.");
+        }
+
+        setIsLoading(true);
+
+        auth()
+        .signInWithEmailAndPassword(email,password)
+        .catch((error) => {
+            console.log(error);
+            setIsLoading(false);
+
+            if(error.code === "auth/invalid-email"){
+                return Alert.alert("Entrar", "E-mail inválido.");
+            }
+
+            if(error.code === "auth/wrong-password"){
+                return Alert.alert("Entrar", "E-mail ou senha inválida.");
+            }
+
+            if(error.code === "auth/user-not-found"){
+                return Alert.alert("Entrar", "Usuário não cadastrado.");
+            }
+
+            return Alert.alert("Entrar", "não foi possível acessar.");
+        });
     }
 
 
@@ -30,7 +58,7 @@ export function SignIn() {
                 mb={4}
                 placeholder="E-mail"
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-                onChangeText={setName}
+                onChangeText={setEmail}
             />
             <Input
                 mb={8}
@@ -40,7 +68,12 @@ export function SignIn() {
                 onChangeText={setPassword}
             />
 
-            <Button title="Entrar" w="full" onPress={handleSignIn} />
+            <Button 
+            title="Entrar" 
+            w="full" 
+            onPress={handleSignIn} 
+            isLoading={isLoading}
+            />
         </VStack>
     )
 }
