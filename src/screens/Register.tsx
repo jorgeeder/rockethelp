9c9c9f6d98b976c041f58068b6f449803f489960
jorgeeder <jorgeeder.js@gmail.com>
2022-07-23 00:38:38 -0300

    feat: submission of registration to make a new maintenance request

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,31 +1,72 @@
+import { useState } from 'react';
 import { VStack } from 'native-base';
+import { useNavigation } from "@react-navigation/native";
 
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { Alert } from 'react-native';
+import firestore from "@react-native-firebase/firestore";
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert("Registrar", "Preencha todos campos")
+    }
+
+    setIsLoading(true);
+
+    firestore()
+      .collection("orders")
+      .add({
+        patrimony,
+        description,
+        status: "open",
+        created_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert("Solicitação", "solicitação registrada com sucesso.");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert("Solicitação", "Não foi possivel registrar o pedido");
+      });
+  }
+
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
-        <Header title="Nova solicitação"/>
+      <Header title="Nova solicitação" />
 
-        <Input 
-            placeholder="Número do patrimônio"
-            mt={4}
-        />
+      <Input
+        placeholder="Número do patrimônio"
+        mt={4}
+        onChangeText={setPatrimony}
+      />
 
-        <Input 
-            placeholder="Descrição do problema"
-            flex={5}
-            mt={5}
-            multiline
-            textAlignVertical="top"
-        />
+      <Input
+        placeholder="Descrição do problema"
+        flex={5}
+        mt={5}
+        multiline
+        textAlignVertical="top"
+        onChangeText={setDescription}
+      />
 
-        <Button 
-            title="Cadastrar"
-            mt={5}
-        />
+      <Button
+        title="Cadastrar"
+        mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+      />
     </VStack>
   );
 }
